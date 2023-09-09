2f9984fb8386f7c23f7df983dc4d35ed2805ed17
wardah <119267583+wardahsharif@users.noreply.github.com>
2023-09-09 13:58:54 +0300

    made some changes

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu");
+
+    menuToggle.addEventListener("click", function () {
+        menu.classList.toggle("active");
+    });
+});
+
+
